5349bbc9bd7dfa3596027fe1a22eb38487d8eb31
MatroskinKot88 <54124590+MatroskinKot88@users.noreply.github.com>
2026-06-22 21:33:47 +0300

    пересоздал файл start.sh

--- backend/start.sh
+++ backend/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# Запускаем FastAPI в фоновом режиме
+echo "Starting FastAPI..."
 uvicorn app.main:app --host 0.0.0.0 --port 8000 &
 
-# Запускаем Telegram бота в основном потоке
+echo "Starting Telegram Bot..."
 python -m app.bot
